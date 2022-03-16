47f03371d0d0d51210d129ba2f577454072ffaae
Kibtiyatu Mohammed Faruk <faruk.kibtiya1@gmail.com>
2022-03-16 14:26:50 +0000

    React-component-week 2-Day 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import{ Navbar } from "./components/Navbar";
+import{ Paragraph } from "./components/Paragraph";
+import{ Input} from "./components/Input";
+import{ MySelf } from "./components/Myself";
+import{ Counter } from "./components/counter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return <div className="App">
+      <Navbar />
+      <Paragraph />
+      <Input />
+      <MySelf name="Kibtiya"  age={20} gender="female" />
+      <MySelf name="John"  age={21} gender="male" />
+      <MySelf name="kofi"  age={19} gender="male" />
+      <MySelf name="Maria"  age={20} gender="female" />
+      <MySelf name="Kibty"  age={40} gender="female" />
+      <Counter />
+    </div>;
+  }
 
 export default App;
---
+++ src/components/Paragraph.js
@@ -0,0 +1,4 @@
+export const Paragraph = () => {
+    return <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro fugit eos illo numquam esse, quo vitae ipsa, incidunt in ex odio iure molestias modi repellat assumenda debitis! Sint, nisi officiis.</p>;
+
+};   
